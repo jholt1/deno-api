1bc944773e729c7ef946e0b202bcf5c2d8191509
Jonathan Holt <jonathan.a.holt@gmail.com>
2020-07-16 00:22:10 +0100

    json

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,3 @@
-import { ServerRequest } from 'https://deno.land/std@0.61.0/http/server.ts';
-
-export default async (req) => {
-	req.respond({ body: `Hello, from Deno v${Deno.version.deno}!` });
+export default (req) => {
+	req.respond({ body: 'Hello from JavaScript in Deno!' });
 };
