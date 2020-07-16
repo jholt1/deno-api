2c4cfd1d8f34bfa254cccd8f5275d1050d116dc5
Jonathan Holt <jonathan.a.holt@gmail.com>
2020-07-16 16:35:29 +0100

    deno example

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,28 @@
+import { qs } from "https://deno.land/x/qs/mod.ts";
+import { isEmoji } from "https://raw.githubusercontent.com/s1mpson/emoji/master/mod.ts";
+
+const createResponse = (req, body) => {
+	const headers = new Headers();
+
+	headers.set('Content-Type', 'application/json; charset=utf8');
+	req.respond({
+		status: 200,
+		headers,
+		body: JSON.stringify(body, null, 2)
+	});
+};
+
 export default (req) => {
-	req.respond({ body: 'Hello from JavaScript in Deno!' });
+	const { url } = req;
+	const query = decodeURIComponent(url.split('?')[1]);
+	const params = qs.parse(query);
+	const emoji = params.emoji ? isEmoji(params.emoji) : false;
+
+	const body = {
+		name: "Jon Holt",
+		email: "jon@example.com",
+		emoji
+	};
+
+	createResponse(req, body);
 };
--- api/welcome.js
+++
@@ -1,7 +0,0 @@
-import { serve } from 'https://deno.land/std/http/server.ts'
-
-console.log('server is running on port 3000');
-
-for await (const req of serve({ port: 3000 })) {
-    req.respond({ body: '<h1>Welcome to deno land</h1>' });
-}
